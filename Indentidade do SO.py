8394ff90470b57a43ffcbfaf7d03d7b53995932b
Lucas Patrick Ramos de Oliveira <lpro@discente.ifpe.edu.br>
2024-07-09 15:59:30 -0300

    IdentificadorSO

---
+++ Indentidade do SO.py
@@ -0,0 +1,7 @@
+import platform 
+print('Nome da máquina: ' ,platform.node())
+print('Arquitetura: ',platform.architecture())
+print('Sistema Operacional: ' ,platform.system())
+print('Versão do SO: ' ,platform.release())
+print('Processador: ' ,platform.processor())
+print('Versão do Python: ',platform.python_version())
